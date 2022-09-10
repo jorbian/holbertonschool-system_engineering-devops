8b0d0ef597425f886aa204f23cb53009a5b69b68
jorbian <60049930+jorbian@users.noreply.github.com>
2022-09-10 01:21:13 -0500

    Actually keeping up one my homework with this sprint is kind of awsome. It certainly helps that there's actual coding involed an not trying to get the exactly one answer the checker will accept. The inital exictment at soming to holberton is begining to return.

--- test_regex1.py
+++
@@ -1,8 +0,0 @@
-import re
-
-p = re.compile(ur'(\b[\d])x(\d{2})-(.*)')
-test_str = u"0x03-shell_variables_expansions\n0x01-emacs\n0x02-vi"
-    
-matches = re.findall(p, test_str)
-
-print(matches[0][0])
