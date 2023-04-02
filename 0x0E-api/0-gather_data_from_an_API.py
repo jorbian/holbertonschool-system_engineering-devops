83f93565d1bec0e488eb557e0b2afafa0f6f86e4
jorbian <60049930+jorbian@users.noreply.github.com>
2023-04-02 16:14:16 -0500

    what I have with everything so far

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -1,1 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    packet_URL = (
+        "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])
+    )
+    data_packet = requests.get(packet_URL)
+
+    print(data_packet.text)
