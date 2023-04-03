9ad7b4cc81eba79c627c088179e91f1286550719
jorbian <60049930+jorbian@users.noreply.github.com>
2023-04-03 16:59:19 -0500

    Third task

--- 0x0E-api/2-export_to_JSON.py
+++ 0x0E-api/2-export_to_JSON.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python3
-
+"""EXPORTS TODO LIST FOR GIVEN EMPLOYEE TO JSON FORMAT"""
+import json
 import requests
 import sys
 
 if __name__ == "__main__":
-    pass
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump(
+            {
+                user_id: [
+                    {
+                        "task": t.get("title"),
+                        "completed": t.get("completed"),
+                        "username": username
+                    } for t in todos
+                ]
+            },
+            jsonfile
+        )
